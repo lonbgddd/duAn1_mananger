410c37319daf123f1fa4b7976dddce9275e2c19c
LUC ANH TAI <93418649+tailucanh@users.noreply.github.com>
2022-11-12 00:01:08 +0700

    lucanhtai update aniview login

--- app/src/main/java/com/example/duan1_mananger/ui/LoginActivity.java
+++ app/src/main/java/com/example/duan1_mananger/ui/LoginActivity.java
@@ -2,22 +2,46 @@ package com.example.duan1_mananger.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.duan1_mananger.R;
-import com.example.duan1_mananger.databinding.ActivityLoginBinding;
+
+import com.example.duan1_mananger.databinding.ActivitySignInBinding;
+
 
 public class LoginActivity extends AppCompatActivity {
-    private ActivityLoginBinding binding = null;
+    private ActivitySignInBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setBindingAnimation(binding);
 
         binding.tvSignUp.setOnClickListener(v -> {
             startActivity(new Intent(this,SignUpActivity.class));
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         });
+    }
+
+    private void setBindingAnimation(ActivitySignInBinding binding ){
+        viewAnimation(binding.imgLogin,"translationY", -400f, 0f);
+        viewAnimation(binding.tvTitle,"translationY", -400f, 0f);
+        viewAnimation(binding.numberPhone,"translationX", -300f, 0f);
+        viewAnimation(binding.passwword,"translationX", 300f, 0f);
+        viewAnimation(binding.cavButton,"translationY", 400f, 0f);
+        viewAnimation(binding.tvContent,"translationX", -200f, 0f);
+        viewAnimation(binding.tvSignUp,"translationX", 200f, 0f);
+    }
+
+
+    private void viewAnimation(View view, String ani, float... values){
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view,ani,values);
+        animator.setDuration(1500);
+        animator.start();
+
     }
 }
--- app/src/main/java/com/example/duan1_mananger/ui/SignUpActivity.java
+++ app/src/main/java/com/example/duan1_mananger/ui/SignUpActivity.java
@@ -3,14 +3,17 @@ package com.example.duan1_mananger.ui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.duan1_mananger.MainActivity;
 import com.example.duan1_mananger.R;
+import com.example.duan1_mananger.databinding.ActivitySignInBinding;
 import com.example.duan1_mananger.databinding.ActivitySignUpBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,6 +28,7 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setBindingAnimation(binding);
         progressDialog = new ProgressDialog(this);
         binding.btnSignin.setOnClickListener(v -> {
             createUser();
@@ -35,7 +39,7 @@ public class SignUpActivity extends AppCompatActivity {
     private void createUser(){
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         progressDialog.show();
-        mAuth.createUserWithEmailAndPassword(binding.email.getText().toString(), binding.pass.getText().toString())
+        mAuth.createUserWithEmailAndPassword(binding.numberPhone.getText().toString(), binding.pass.getText().toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -52,4 +56,37 @@ public class SignUpActivity extends AppCompatActivity {
                 });
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+    }
+    private void setBindingAnimation(ActivitySignUpBinding binding ){
+        viewAnimation(binding.immSignUp,"translationY", -400f, 0f);
+        viewAnimation(binding.tvTitle,"translationY", -400f, 0f);
+        viewAnimation(binding.numberPhone,"translationX", -400f, 0f);
+        viewAnimation(binding.pass,"translationX", 400f, 0f);
+        viewAnimation(binding.rePass,"translationX", -400f, 0f);
+        viewAnimation(binding.cavButton,"translationY", 400f, 0f);
+
+    }
+
+    private void viewAnimation(View view, String ani, float... values){
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view,ani,values);
+        animator.setDuration(1500);
+        animator.start();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
