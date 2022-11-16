252a2ab78fd161582de8109262de21ad6920f599
pc <long0388209521@gmail.com>
2022-11-16 10:58:39 +0700

    create user with email

---
+++ app/src/main/java/com/example/duan1_mananger/model/User.java
@@ -0,0 +1,97 @@
+package com.example.duan1_mananger.model;
+
+
+public class User {
+    private int id;
+    private String name_user;
+    private String avatar;
+    private Boolean sex = true;
+    private String phone_number;
+    private String birthday;
+    private String address;
+    private String password;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName_user() {
+        return name_user;
+    }
+
+    public void setName_user(String name_user) {
+        this.name_user = name_user;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String name_user, String avatar, Boolean sex, String phone_number, String birthday, String address, String password, String email) {
+        this.name_user = name_user;
+        this.avatar = avatar;
+        this.sex = sex;
+        this.phone_number = phone_number;
+        this.birthday = birthday;
+        this.address = address;
+        this.password = password;
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/duan1_mananger/ui/SignUpActivity.java
+++ app/src/main/java/com/example/duan1_mananger/ui/SignUpActivity.java
@@ -7,16 +7,22 @@ import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.duan1_mananger.R;
 import com.example.duan1_mananger.databinding.ActivitySignUpBinding;
+import com.example.duan1_mananger.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.Serializable;
 
 public class SignUpActivity extends AppCompatActivity {
     private ActivitySignUpBinding binding = null;
@@ -29,7 +35,7 @@ public class SignUpActivity extends AppCompatActivity {
         setBindingAnimation(binding);
 
         binding.btnSignup.setOnClickListener(v -> {
-            if(validateInput()){
+            if (validateInput()) {
                 createUser();
             }
 
@@ -37,7 +43,8 @@ public class SignUpActivity extends AppCompatActivity {
 
 
     }
-    private void createUser(){
+
+    private void createUser() {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         binding.progressBar.setVisibility(View.VISIBLE);
         binding.layoutSignUp.setAlpha(0.2f);
@@ -47,11 +54,32 @@ public class SignUpActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if (task.isSuccessful()) {
+                            User user = new User(" ",
+                                    "",
+                                    true,
+                                    "",
+                                    "",
+                                    "",
+                                    binding.pass.getText().toString(),
+                                    binding.email.getText().toString());
+                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+                            reference.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d("TAG", "onComplete: " + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                                Toast.makeText(SignUpActivity.this, getString(R.string.notifi_sign_up_success), Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                                                binding.progressBar.setVisibility(View.VISIBLE);
+                                                finishAffinity();
+                                            }
+                                        }
+                                    });
+
                             // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(SignUpActivity.this, getString(R.string.notifi_sign_up_success), Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
-                            binding.progressBar.setVisibility(View.VISIBLE);
-                            finishAffinity();
+
                         } else {
                             Toast.makeText(SignUpActivity.this, getString(R.string.notifi_sign_up_fail), Toast.LENGTH_SHORT).show();
                             binding.progressBar.setVisibility(View.GONE);
@@ -61,44 +89,44 @@ public class SignUpActivity extends AppCompatActivity {
                 });
     }
 
-    private boolean validateInput(){
+    private boolean validateInput() {
         String strEmail = binding.email.getText().toString().trim();
         String strPass = binding.pass.getText().toString().trim();
         String strRePass = binding.rePass.getText().toString().trim();
-        if(TextUtils.isEmpty(strEmail)){
-            binding.email.setError(getString(R.string.error_email_1),null);
+        if (TextUtils.isEmpty(strEmail)) {
+            binding.email.setError(getString(R.string.error_email_1), null);
             binding.email.requestFocus();
             return false;
-        }else if(!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
-            binding.email.setError(getString(R.string.error_email_2),null);
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+            binding.email.setError(getString(R.string.error_email_2), null);
             binding.email.requestFocus();
             return false;
-        }else if(TextUtils.isEmpty(strPass)) {
-            binding.pass.setError(getString(R.string.error_pass_1),null);
+        } else if (TextUtils.isEmpty(strPass)) {
+            binding.pass.setError(getString(R.string.error_pass_1), null);
             binding.pass.requestFocus();
             return false;
-        } else if(!(strRePass.matches(strPass)) || TextUtils.isEmpty(strRePass)){
-            binding.rePass.setError(getString(R.string.error_pass_2),null);
+        } else if (!(strRePass.matches(strPass)) || TextUtils.isEmpty(strRePass)) {
+            binding.rePass.setError(getString(R.string.error_pass_2), null);
             binding.rePass.requestFocus();
             return false;
-        }else {
+        } else {
             return true;
         }
 
     }
 
-    private void setBindingAnimation(ActivitySignUpBinding binding ){
-        viewAnimation(binding.immSignUp,"translationY", -400f, 0f);
-        viewAnimation(binding.tvTitle,"translationY", -400f, 0f);
-        viewAnimation(binding.email,"translationX", -400f, 0f);
-        viewAnimation(binding.tilPass,"translationX", 400f, 0f);
-        viewAnimation(binding.tilRePass,"translationX", -400f, 0f);
-        viewAnimation(binding.cavButton,"translationY", 400f, 0f);
+    private void setBindingAnimation(ActivitySignUpBinding binding) {
+        viewAnimation(binding.immSignUp, "translationY", -400f, 0f);
+        viewAnimation(binding.tvTitle, "translationY", -400f, 0f);
+        viewAnimation(binding.email, "translationX", -400f, 0f);
+        viewAnimation(binding.tilPass, "translationX", 400f, 0f);
+        viewAnimation(binding.tilRePass, "translationX", -400f, 0f);
+        viewAnimation(binding.cavButton, "translationY", 400f, 0f);
 
     }
 
-    private void viewAnimation(View view, String ani, float... values){
-        ObjectAnimator animator = ObjectAnimator.ofFloat(view,ani,values);
+    private void viewAnimation(View view, String ani, float... values) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, ani, values);
         animator.setDuration(1500);
         animator.start();
     }
